18f2fd18d8d263ae193420d80458f2d28924ab71
Taufik Pragusga <pragusga25@gmail.com>
2021-12-09 11:09:41 +0700

    init

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:invid19_mobile/main.dart';
+import 'package:invid19/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
